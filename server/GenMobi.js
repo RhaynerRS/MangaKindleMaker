c5b1f8ca8c9cbeea96cb64eea159fb502640db38
rossmann <rhayner3301@gmail.com>
2023-04-21 19:21:24 -0300

    separa front do back

--- server/GenMobi.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require('child_process');
-const path = require("path");
-
-async function GenMobi(PathToEpub, NomeArquivo) {
-    exec(`cd ${path.join(__dirname)} & kindlegen.exe ${PathToEpub} -o ${NomeArquivo}.mobi`, () => {});
-}
-
-module.exports = GenMobi;
